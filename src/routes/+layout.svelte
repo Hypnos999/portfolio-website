9eba010ca61729e15d9802b65a9de2e764a1faed
Hypnos999 <flama2204@gmail.com>
2024-10-12 05:27:22 +0200

    commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+
+
+<nav>
+    <a href="/">About Me</a>
+    <a href="/projects">My Projects</a>
+    <a href="/contacts">Get in contact</a>
+</nav>
+
+
+<slot>
+</slot>
+
+<style>
+    
+nav {
+        width: max-content;
+        position: fixed;
+        top: 60px;
+        left: 50%;
+        transform: translateX(-50%);
+        padding: 20px 0;
+        background: #151515;
+        border-radius: 5px;
+        box-shadow:  5px 5px 10px 0px #0c0c0c;
+        z-index: 999;
+    }
+
+    a {
+        /* color: rgba(0, 0, 0, 0.712); */
+        /* background: #151515; */
+        /* text-transform: uppercase; */
+        /* font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; */
+        /* font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; */
+        /* font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; */
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        text-decoration: none;
+        color: #aaa;
+        font-weight: 700;
+        font-size: 26px;
+        padding: 20px 40px;
+        border-radius: 5px;
+        transition: 0.33s;
+    }
+
+    a:hover {
+        /* background: rgb(122, 210, 14); */
+        background: #6a040f;
+        color: #eee;
+        padding: 20px 42px;
+    }
+
+    nav > a:nth-child(2) {
+        /* margin: 0 15px; */
+        margin: 0;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,183 @@
+<script>
+    import selfie from "$lib/file1.png"
+    import its_logo from "$lib/its_logo.png"
+</script>
+
+<section>
+    <div id="first-pannel">
+        <div id="foto">
+            <img src={selfie} alt="A photo of myself">
+        </div>
+    
+        <nav>
+            <h1>Hi, i'm <h1 class="dark-text">Flavio Manna</h1></h1>
+            <p>Mi chiamo Flavio Manna e sono un
+                giovane studente e praticante nel
+                campo del Machine Learning.
+                Ciò che mi stimola e mi appassiona di
+                questo settore è la possibilità di dare
+                vita alle mie idee e di applicarle alla
+                vita di tutti i giorni.
+                Seguire da vicino e partecipare allo
+                sviluppo di questa tecnologia è per
+                me fonte di stimolo e non vedo l’ora
+                di entrare nel mondo del lavoro per
+                apprendere nuove conoscenze e
+                fornire il mio contributo in questo
+                ambito.</p>
+        </nav>
+    
+        <a href="#second-pannel">
+            <div class="arrow">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </a>
+    </div>
+
+    <div id="second-pannel">
+        <a href="#first-pannel">
+            <div class="arrow-down" >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </a>
+
+        
+        <h2>My instruction</h2>
+        <h2>My background</h2>
+        <img id="its-logo" src={its_logo} alt="A logo of the techinal istitute where a studied">
+    </div>
+</section>
+
+
+<style>
+    * {
+        scroll-behavior: smooth;
+    }
+
+    #its-logo {
+        width: 10vw;
+        height: auto;
+        /* height: 10vw; */
+    }
+
+    #first-pannel{
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+    }
+
+    #foto {
+        height: 75vh;
+        position: absolute;
+        bottom: 0;
+        opacity: 0.8;
+        left: 10vw;
+        overflow: hidden;
+    }
+
+    img {
+        height: 85vh;
+        opacity: 0.8;
+    }
+
+    nav {
+        position: absolute;
+        left: 50vw;
+        top: 50vh;
+        transform: translateY(-50%);
+    }
+
+    h1 {
+        font-size: 50px;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        color: #d00000;
+        display: inline-block;
+    }
+
+    .dark-text {
+        color: #6a040f;
+    }
+
+    h1 .dark-text {
+        font-size: 70px;
+    }
+
+    p {
+        font-size: 24px;
+        color: #ddd;
+        width: 40vw;
+        margin-top: 20px;
+    }
+
+    .arrow-down {
+        position: absolute;
+        bottom: 4vh;
+        left: 50%;
+        transform: translateX(-50%) rotate(180deg);
+        /* transform: rotate(180deg) !important; */
+        cursor: pointer;
+    }
+
+    .arrow {
+        position: absolute;
+        bottom:7vh;
+        left: 50%;
+        transform: translateX(-50%);
+        /* transform: rotate(0deg); */
+        cursor: pointer;
+    }
+
+    .arrow span, .arrow-down span {
+        display: block;
+        width: 1.5vw;
+        height: 1.5vw;
+        transform: rotate(45deg);
+        margin: -10px;
+        animation: animate 3s infinite;
+    }
+
+    .arrow span {
+        border-bottom: 5px solid #d00000;
+        border-right: 5px solid #6a040f;
+    }
+
+    .arrow-down span {
+        border-bottom: 5px solid #222;
+        border-right: 5px solid #111;
+    }
+
+    .arrow-down span:nth-child(2), .arrow span:nth-child(2) {
+        animation-delay: -0.2s;
+    }
+
+    .arrow-down span:nth-child(3), .arrow span:nth-child(3) {
+        animation-delay: -0.4s;
+    }
+
+    @keyframes animate {
+        0% {
+            opacity: 0;
+            transform: rotate(45deg) translate(-20px, -20px);
+        }
+        50% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+            transform: rotate(45deg) translate(20px, 20px);
+        }
+    }
+
+    #second-pannel {
+        width: 100vw;
+        height: 100vh;
+        background: #6a040f;
+        position: relative;
+    }
+
+
+</style>
